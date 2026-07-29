55febf9840212b54e012547045b19f343dfca8fd
grapinou <remigrapinou@gmail.com>
2026-07-29 10:20:51 +0200

    adding routes into main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,9 +9,24 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Bienvenue sur Club Manager")
 }
 
+func clubPresentationHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Présentation de Club Manager")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Pour contacter Club Manager")
+}
+
+func rulesHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Réglement de Club Manager")
+}
+
 func main() {
 
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/clubPresentation", clubPresentationHandler)
+	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/rules", rulesHandler)
 
 	fmt.Println("Serveur lancé sur http://localhost:8080")
 
