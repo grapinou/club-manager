a56409d14304f19e51359c3b0cd2f7aa06b04287
grapinou <remigrapinou@gmail.com>
2026-08-04 14:54:39 +0200

    feat: render club page with shared layout

--- internal/handlers/002_club.go
+++ internal/handlers/002_club.go
@@ -1,11 +1,27 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/grapinou/club-manager/internal/views"
 )
 
 func ClubHandler(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprintln(w, "Présentation du club")
+	data := views.ClubData{
+		Title:       "Le club - Club Manager",
+		Heading:     "Présentation du club",
+		Description: "Découvrez l'association, son histoire et ses valeurs.",
+	}
+
+	err := views.RenderClub(w, data)
+
+	if err != nil {
+		http.Error(
+			w,
+			"Erreur interne du serveur",
+			http.StatusInternalServerError,
+		)
+	}
+
 }
