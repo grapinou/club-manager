a9a0947ae32069e6b18f9848d3ddcad2789d96d7
grapinou <remigrapinou@gmail.com>
2026-08-11 15:42:38 +0200

    Connect application to PostgreSQL with pgxpool

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/grapinou/club-manager/internal/config"
 	"github.com/grapinou/club-manager/internal/router"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 const configPath = "config/config.json"
@@ -19,6 +22,28 @@ func main() {
 			err,
 		)
 	}
+
+	ctx := context.Background()
+
+	db, err := pgxpool.New(
+		ctx, os.Getenv("DATABASE_URL"),
+	)
+	if err != nil {
+		log.Fatalf(
+			"impossible de créer le pool PostgreSQL : %v",
+			err,
+		)
+	}
+	defer db.Close()
+
+	if err := db.Ping(ctx); err != nil {
+		log.Fatalf(
+			"impossible de se connecter à PostgreSQL : %v",
+			err,
+		)
+	}
+
+	log.Println("Connexion à PostgreSQL établie")
 
 	mux := router.New(cfg)
 
