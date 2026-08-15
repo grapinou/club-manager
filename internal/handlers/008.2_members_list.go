3bf8aeb2101814a4e5ff5d8c3e5529296c4fd5b6
grapinou <remigrapinou@gmail.com>
2026-08-15 09:41:49 +0200

    prepare member data for view

--- internal/handlers/008.2_members_list.go
+++ internal/handlers/008.2_members_list.go
@@ -3,13 +3,15 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/grapinou/club-manager/internal/config"
 	"github.com/grapinou/club-manager/internal/database"
+	"github.com/grapinou/club-manager/internal/views"
 )
 
-func MembersHandler(queries database.Queries) http.HandlerFunc {
+func MembersHandler(cfg config.Config, queries database.Queries) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		_, err := queries.ListMembers(r.Context())
+		members, err := queries.ListMembers(r.Context())
 		if err != nil {
 			http.Error(
 				w,
@@ -18,5 +20,33 @@ func MembersHandler(queries database.Queries) http.HandlerFunc {
 			)
 			return
 		}
+
+		var memberData []views.MemberData
+
+		for _, member := range members {
+			memberData = append(memberData, views.MemberData{
+				ID:        member.ID,
+				FirstName: member.FirstName,
+				LastName:  member.LastName,
+				BirthDate: member.BirthDate.Time.Format("02/01/2006"),
+				Email:     member.Email.String,
+			})
+		}
+
+		data := views.MembersData{
+			SiteName: cfg.SiteName,
+			Title:    "Membres - " + cfg.SiteName,
+			Members:  memberData,
+		}
+
+		if err := views.RenderMembersList(w, data); err != nil {
+			http.Error(
+				w,
+				"impossible d'afficher la liste des adhérents",
+				http.StatusInternalServerError,
+			)
+			return
+		}
+
 	}
 }
---
+++ internal/views/008.2_members_list.go
@@ -0,0 +1,41 @@
+package views
+
+import (
+	"embed"
+	"html/template"
+	"io"
+)
+
+//go:embed templates/layouts/base.html
+//go:embed templates/pages/008.2_members_list.html
+var membersListFiles embed.FS
+
+type MemberData struct {
+	ID        int32
+	FirstName string
+	LastName  string
+	BirthDate string
+	Email     string
+}
+
+type MembersData struct {
+	SiteName string
+	Title    string
+	Members  []MemberData
+}
+
+var membersListTemplate = template.Must(
+	template.ParseFS(
+		membersListFiles,
+		"templates/layouts/base.html",
+		"tempates/pages/008.2_members_list.html",
+	),
+)
+
+func RenderMembersList(w io.Writer, data MembersData) error {
+	return membersListTemplate.ExecuteTemplate(
+		w,
+		"base",
+		data,
+	)
+}
